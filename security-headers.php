19fe9a3e85233e334672bbf64c2827d78a4c7438
sdamion <sdamion@gmail.com>
2025-03-25 13:02:03 +0100

    tweaks

--- security-headers.php
+++ security-headers.php
@@ -9,27 +9,22 @@ $GLOBALS['csp_nonce'] = $nonce;
 
 // Set security headers
 header("Content-Security-Policy: 
-    default-src 'self'; 
-    script-src 'self' 'nonce-$nonce' 
-        https://api.coingecko.com 
-        https://js.cexplorer.io 
-        https://api.starch.one 
-        https://cdn.jsdelivr.net;
-    style-src 'self' 'nonce-$nonce' 
-        https://fonts.googleapis.com;
-    connect-src 'self' 
-        https://api.coingecko.com 
-        https://js.cexplorer.io 
-        https://api.starch.one 
-        https://www.tdsp.online/api/resource/6738f8cddce9fe405732f092;
-    img-src 'self' data: 
-        https://* 
-        https://cexplorer.io 
-        https://js.cexplorer.io 
-        https://api.starch.one;
+    default-src 'self';
+    base-uri 'self';
+    object-src 'none'; 
+    script-src 'self' 'nonce-$nonce' https://api.coingecko.com https://js.cexplorer.io https://api.starch.one https://cdn.jsdelivr.net;
+    style-src 'self' 'nonce-$nonce' https://fonts.googleapis.com;
+    connect-src 'self' https://api.coingecko.com https://js.cexplorer.io https://api.starch.one https://www.tdsp.online/api/resource/6738f8cddce9fe405732f092;
     font-src 'self' https://fonts.gstatic.com;
     frame-src 'self';
+    form-action 'self';
+    require-trusted-types-for 'script';
+    frame-ancestors 'none';
 ");
-
+header("X-Frame-Options: DENY"); // Prevent clickjacking
+header("X-Content-Type-Options: nosniff"); // Prevent MIME sniffing
+header("Referrer-Policy: strict-origin-when-cross-origin");
+header("Permissions-Policy: geolocation=(), microphone=(), camera=()"); // Block unused APIs
+header("Strict-Transport-Security: max-age=63072000; includeSubDomains; preload"); // HSTS
 header("Cache-Control: max-age=120, must-revalidate");
 ?>
