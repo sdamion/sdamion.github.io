6333cf10a83861e7819ce2312f6566f73024d237
DamionDutch <sdamion@gmail.com>
2025-04-07 15:19:50 +0200

    add iframe to index and about for stake now

---
+++ stakenow.js
@@ -0,0 +1,8 @@
+function openModal(event) {
+    event.preventDefault();
+    document.getElementById("iframeModal").style.display = "block";
+  }
+
+  function closeModal() {
+    document.getElementById("iframeModal").style.display = "none";
+  }
